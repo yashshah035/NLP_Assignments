f17338568093d242ae26b49fed42ae8195ba80ae
yashshah035 <yashshah035@gmail.com>
2024-04-23 20:54:44 +0530

    changed epochs to 3

--- assignment4.py
+++ assignment4.py
@@ -1,9 +1,7 @@
 import torch
 import torch.nn as nn
 import torch.optim as optim
-import torch.utils.data as data
 import math
-import copy
 
 class MultiHeadAttention(nn.Module):
     def __init__(self, d_model, num_heads):
@@ -180,7 +178,7 @@ optimizer = optim.Adam(transformer.parameters(), lr=0.0001, betas=(0.9, 0.98), e
 
 transformer.train()
 
-for epoch in range(100):
+for epoch in range(3):
     optimizer.zero_grad()
     output = transformer(src_data, tgt_data[:, :-1])
     loss = criterion(output.contiguous().view(-1, tgt_vocab_size), tgt_data[:, 1:].contiguous().view(-1))
